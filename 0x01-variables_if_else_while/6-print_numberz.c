5493df550ea8a56cc399e234cf0615795328ba18
Philip Mensah <nukem8475@gmail.com>
2023-06-16 14:27:06 -0700

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-	char digits[] = "0123456789\n";
-	
 	int i;
-	for (i = 0; i < 11; i++)
+
+	for (i = 48; i < 58; i++)
 	{
-		putchar(digits[i]);
+		putchar(i);
 	}
+	putchar('\n');
 	return (0);
 }
+
