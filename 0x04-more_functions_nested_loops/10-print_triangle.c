0fc7d6c70a6ae0c5b3f911521d784cde20c0213b
Philip Mensah <nukem8475@gmail.com>
2023-06-23 01:59:39 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, spaces;
 
 	if (size <= 0)
 	{
@@ -16,6 +16,11 @@ void print_triangle(int size)
 	}
 	for (i = 0; i < size; i++)
 	{
+		spaces = size - 1 - i;
+		for (j = 0; j < spaces; j++)
+		{
+			_putchar(' ');
+		}
 		for (j = 0; j <= i; j++)
 		{
 			_putchar('#');
