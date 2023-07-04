3fe93c8b96314b85526733f884be00ae326d085f
Philip <nukem8475@gmail.com>
2023-07-04 15:34:48 -0700

    m

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+char *_strpbrk(char *s, char *accept)
+{
+    if (s == NULL || accept == NULL)
+        return NULL;
+
+    while (*s != '\0')
+    {
+        char *a = accept;
+        while (*a != '\0')
+        {
+            if (*s == *a)
+                return s;
+            a++;
+        }
+        s++;
+    }
+
+    return NULL;
+}
