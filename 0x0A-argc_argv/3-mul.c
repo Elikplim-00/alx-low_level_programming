d66254d9d5881be197c33633a74c70ee34f97bd4
Philip <nukem8475@gmail.com>
2023-07-08 08:14:50 -0700

    m

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point of the program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
  *
- * Return: Always 0
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int num1, num2, result;
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		printf("Usage: %s <num1> <num2>\n", argv[0]);
+		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
