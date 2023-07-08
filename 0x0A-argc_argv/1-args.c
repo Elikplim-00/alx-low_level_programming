cfca62ef181708e0dcd393717bf31ba98c5cb152
Philip <nukem8475@gmail.com>
2023-07-08 08:04:54 -0700

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = argc - 1;
+
+	printf("%d\n", count);
+	return (0);
+}
+
