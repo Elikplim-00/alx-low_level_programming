faf918bc64512e17598c76876f2308f71036758a
Philip <nukem8475@gmail.com>
2023-07-17 15:11:18 -0700

    m

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/*
+ * main - It prints the name of the source file
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,17 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - Computes the sum of the numbers x and y.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Description: This macro takes two numbers x and y as input and returns their sum.
+ * It simply adds the two numbers together using the `+` operator.
+ *
+ * Return: The sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_MACRO_H */
+
