fc45850da1601ddeac48d1a8fef282608d827948
Philip <nukem8475@gmail.com>
2023-07-19 05:31:59 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,5 @@ typedef struct dog
 } dog_t;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(const dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
